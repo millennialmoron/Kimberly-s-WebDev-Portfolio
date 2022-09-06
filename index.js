85f273182136f48d24f9707296c8790b14b48b90
millennialmoron <millennialmoron87@gmail.com>
2022-09-06 15:11:49 +0200

    trying to fix index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+alert("hey bitch");
+
 var scale = chroma
   .scale(["#004097", "#FD386C"])
   .domain([0, $(document).height()]);
 $(window).on("scroll", function () {
   $("body").css("background-color", scale(window.pageYOffset));
 });
+
+//figure out why background isn't working. needs listener? needs an ass kicking? needs me to throw laptop from balcony?...the possibilities are endless.
