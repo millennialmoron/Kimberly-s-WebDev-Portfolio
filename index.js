ab9d129f2bdd4d62a79bc1c35c47744ab6209060
millennialmoron <millennialmoron87@gmail.com>
2022-09-21 12:51:38 +0200

    fixing background loading issue

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ var scale = chroma
   .scale(["#004097", "#FD386C"])
   .domain([0, $(document).height()]);
 $(window).on("scroll", function () {
-  $("body").css("background-color", scale(window.pageYOffset));
+  $("body").css("background-color");
 });
+
+// scale(window.pageYOffset)
